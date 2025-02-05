bb4184e89623d9b76b3546230afddf5b480d6f10
keij <skakovadrawing@gmail.com>
2025-02-05 20:31:33 +0300

    storage

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,20 +2,54 @@ package main
 
 import (
 	"UrlShort/internal/config"
-	"fmt"
+	"UrlShort/internal/lib/logger/sl"
+	"UrlShort/internal/storage/sqlite"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 
-	//init config
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
-	//init logger
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
 
-	//init storage
+	log.Info("initializing server", slog.String("address", cfg.Address))
+	log.Debug("logger debug mode enabled")
 
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 	//init router
 
 	//run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -9,31 +9,33 @@ import (
 )
 
 type Config struct {
-	Env         string `yaml:"env" env-default:"local"`
+	Env         string `yaml:"env" env-default:"development"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"8080"`
-	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
 	configPath := os.Getenv("CONFIG_PATH")
 	if configPath == "" {
-		log.Fatal("CONFIG_PATH is not set")
+		log.Fatal("CONFIG_PATH environment variable is not set")
 	}
 
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config file does not exists: %s", configPath)
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("error opening config file: %s", err)
 	}
 
 	var cfg Config
 
-	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		log.Fatalf("cannot read config: %s", err)
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("error reading config file: %s", err)
 	}
+
 	return &cfg
 }
